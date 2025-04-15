f8f8b29f1f78552643f91f69e874ac669ff0e8ca
anastasia_ule <89247750950nasta@gmail.com>
2025-04-15 13:12:37 +0800

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static система_частиц.Emitter;
+using static система_частиц.Particle;
+
+namespace система_частиц
+{
+    public partial class Form1 : Form
+    {
+
+        List<Emitter> emitters = new List<Emitter>();
+        Emitter emitter;
+
+        public Form1()
+        {
+            InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            this.emitter = new Emitter // создаю эмиттер и привязываю его к полю emitter
+            {
+                Direction = 0,
+                Spreading = 10,
+                SpeedMin = 10,
+                SpeedMax = 10,
+                ColorFrom = Color.Gold,
+                ColorTo = Color.FromArgb(0, Color.Red),
+                ParticlesPerTick = 10,
+                X = picDisplay.Width / 2,
+                Y = picDisplay.Height / 2,
+            };
+
+            emitters.Add(this.emitter); // все равно добавляю в список emitters, чтобы он рендерился и обновлялся
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black);
+                emitter.Render(g); // а тут теперь рендерим через эмиттер
+            }
+
+            picDisplay.Invalidate();
+        }
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // а тут в эмиттер передаем положение мыфки
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
+        }
+    }
+}
